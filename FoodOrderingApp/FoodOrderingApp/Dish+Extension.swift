9a12ce8a1898472b3a403360a356c406b2ccfe5e
rhencyd <114204612+rhencyd@users.noreply.github.com>
2023-03-24 22:53:36 -0500

    Delete FoodOrderingApp directory

--- FoodOrderingApp/FoodOrderingApp/Dish+Extension.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Dish+Extension.swift
-//  FoodOrderingApp
-//
-//  Created by Rhency Delgado on 3/13/23.
-//
-
-import Foundation
--- FoodOrderingApp/FoodOrderingApp/DishesModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DishesModel.swift
-//  FoodOrderingApp
-//
-//  Created by Rhency Delgado on 3/13/23.
-//
-
-import Foundation
